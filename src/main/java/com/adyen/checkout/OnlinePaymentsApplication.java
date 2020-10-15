8525be3f5d5fc6d8ec49f1f6ae3ad607ad957d06
deepu105 <d4udts@gmail.com>
2020-10-15 15:42:49 +0200

    init using spring initializer

---
+++ src/main/java/com/adyen/checkout/OnlinePaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.adyen.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adyen/checkout/OnlinePaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adyen.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
