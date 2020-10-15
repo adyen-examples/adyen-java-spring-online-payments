7c0e39760db28c0689e5857b17c093d7ed0e31a1
deepu105 <d4udts@gmail.com>
2020-10-15 16:01:57 +0200

    add log to app

--- src/main/java/com/adyen/checkout/OnlinePaymentsApplication.java
+++ src/main/java/com/adyen/checkout/OnlinePaymentsApplication.java
@@ -1,13 +1,20 @@
 package com.adyen.checkout;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlinePaymentsApplication {
+    private static final Logger log = LoggerFactory.getLogger(OnlinePaymentsApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlinePaymentsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinePaymentsApplication.class, args);
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:8080\n\t" +
+                "\n----------------------------------------------------------");
+    }
 
 }
