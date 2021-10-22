518d6a026e4d98d835305f529b56322a0e9d09d0
Julien Lengrand-Lambert <julien@lengrand.fr>
2021-10-22 10:01:40 +0200

    Starts using sessions

--- src/main/java/com/adyen/checkout/api/CheckoutResource.java
+++ src/main/java/com/adyen/checkout/api/CheckoutResource.java
@@ -1,22 +1,23 @@
 package com.adyen.checkout.api;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.UUID;
-import javax.servlet.http.HttpServletRequest;
+import com.adyen.Client;
+import com.adyen.enums.Environment;
+import com.adyen.model.Amount;
+import com.adyen.model.checkout.CreateCheckoutSessionRequest;
+import com.adyen.model.checkout.CreateCheckoutSessionResponse;
+import com.adyen.service.Checkout;
+import com.adyen.service.exception.ApiException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
-import com.adyen.Client;
-import com.adyen.enums.Environment;
-import com.adyen.model.Amount;
-import com.adyen.model.checkout.*;
-import com.adyen.service.Checkout;
-import com.adyen.service.exception.ApiException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * REST controller for using Adyen checkout API
@@ -36,157 +37,22 @@ public class CheckoutResource {
         this.checkout = new Checkout(client);
     }
 
-    /**
-     * {@code POST  /getPaymentMethods} : Get valid payment methods.
-     *
-     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body the paymentMethods response.
-     * @throws IOException  from Adyen API.
-     * @throws ApiException from Adyen API.
-     */
-    @PostMapping("/getPaymentMethods")
-    public ResponseEntity<PaymentMethodsResponse> paymentMethods() throws IOException, ApiException {
-        var paymentMethodsRequest = new PaymentMethodsRequest();
-        paymentMethodsRequest.setMerchantAccount(merchantAccount);
-        paymentMethodsRequest.setChannel(PaymentMethodsRequest.ChannelEnum.WEB);
-
-        log.info("REST request to get Adyen payment methods {}", paymentMethodsRequest);
-        var response = checkout.paymentMethods(paymentMethodsRequest);
-        return ResponseEntity.ok()
-            .body(response);
-    }
-
-    /**
-     * {@code POST  /initiatePayment} : Make a payment.
-     *
-     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body the paymentMethods response.
-     * @throws IOException  from Adyen API.
-     * @throws ApiException from Adyen API.
-     */
-    @PostMapping("/initiatePayment")
-    public ResponseEntity<PaymentsResponse> payments(@RequestBody PaymentsRequest body, HttpServletRequest request) throws IOException, ApiException {
-        var paymentRequest = new PaymentsRequest();
-        paymentRequest.setMerchantAccount(merchantAccount); // required
-        paymentRequest.setChannel(PaymentsRequest.ChannelEnum.WEB); // required
-
-        var amount = new Amount()
-            .currency(findCurrency(body.getPaymentMethod().getType()))
-            .value(1000L); // value is 10€ in minor units
-        paymentRequest.setAmount(amount);
-
+    @PostMapping("/sessions")
+    public ResponseEntity<CreateCheckoutSessionResponse> sessions(@RequestParam String type) throws IOException, ApiException {
         var orderRef = UUID.randomUUID().toString();
-        paymentRequest.setReference(orderRef); // required
-        // required for 3ds2 redirect flow
-        paymentRequest.setReturnUrl("http://localhost:8080/api/handleShopperRedirect?orderRef=" + orderRef);
+        var amount = new Amount()
+            .currency("EUR")
+            .value(1000L); // value is 10€ in minor units
 
-        // required for 3ds2 native flow
-        paymentRequest.setAdditionalData(Collections.singletonMap("allow3DS2", "true"));
-        // required for 3ds2 native flow
-        paymentRequest.setOrigin("http://localhost:8080");
-        // required for 3ds2
-        paymentRequest.setBrowserInfo(body.getBrowserInfo());
-        // required by some issuers for 3ds2
-        paymentRequest.setShopperIP(request.getRemoteAddr());
+        var checkoutSession = new CreateCheckoutSessionRequest();
+        checkoutSession.merchantAccount(merchantAccount);
+        checkoutSession.setChannel(CreateCheckoutSessionRequest.ChannelEnum.WEB);
+        checkoutSession.setReference(orderRef); // required
+        checkoutSession.setReturnUrl("http://localhost:8080/checkout?type=" + type +"&orderRef=" + orderRef);
+        checkoutSession.setAmount(amount);
 
-        paymentRequest.setPaymentMethod(body.getPaymentMethod());
-
-        var type = body.getPaymentMethod().getType();
-        // required for Klarna
-        if (type.contains("klarna")) {
-            paymentRequest.setCountryCode("DE");
-            paymentRequest.setShopperReference("1234");
-            paymentRequest.setShopperEmail("youremail@email.com");
-            paymentRequest.setShopperLocale("en_US");
-            var lineItems = new ArrayList<LineItem>();
-            lineItems.add(
-                new LineItem().quantity(1L).amountExcludingTax(331L).taxPercentage(2100L).description("Sunglasses").id("Item 1").taxAmount(69L).amountIncludingTax(400L)
-            );
-            lineItems.add(
-                new LineItem().quantity(2L).amountExcludingTax(248L).taxPercentage(2100L).description("Headphones").id("Item 2").taxAmount(52L).amountIncludingTax(300L)
-            );
-            paymentRequest.setLineItems(lineItems);
-        } else if (type.contains("paypal")) {
-            paymentRequest.setCountryCode("US");
-        }
-
-        log.info("REST request to make Adyen payment {}", paymentRequest);
-        var response = checkout.payments(paymentRequest);
-        return ResponseEntity.ok()
-            .body(response);
+        log.info("REST request to create Adyen Payment Session {}", checkoutSession);
+        var response = checkout.sessions(checkoutSession);
+        return ResponseEntity.ok().body(response);
     }
-
-    /**
-     * {@code POST  /submitAdditionalDetails} : Make a payment.
-     *
-     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body the paymentMethods response.
-     * @throws IOException  from Adyen API.
-     * @throws ApiException from Adyen API.
-     */
-    @PostMapping("/submitAdditionalDetails")
-    public ResponseEntity<PaymentsDetailsResponse> payments(@RequestBody PaymentsDetailsRequest detailsRequest) throws IOException, ApiException {
-        log.info("REST request to make Adyen payment details {}", detailsRequest);
-        var response = checkout.paymentsDetails(detailsRequest);
-        return ResponseEntity.ok()
-            .body(response);
-    }
-
-    /**
-     * {@code GET  /handleShopperRedirect} : Handle redirect during payment.
-     *
-     * @return the {@link RedirectView} with status {@code 302}
-     * @throws IOException  from Adyen API.
-     * @throws ApiException from Adyen API.
-     */
-    @GetMapping("/handleShopperRedirect")
-    public RedirectView redirect(@RequestParam(required = false) String payload, @RequestParam(required = false) String redirectResult, @RequestParam String orderRef) throws IOException, ApiException {
-        var detailsRequest = new PaymentsDetailsRequest();
-        if (redirectResult != null && !redirectResult.isEmpty()) {
-            detailsRequest.setDetails(Collections.singletonMap("redirectResult", redirectResult));
-        } else if (payload != null && !payload.isEmpty()) {
-            detailsRequest.setDetails(Collections.singletonMap("payload", payload));
-        }
-
-        return getRedirectView(detailsRequest);
-    }
-
-    private RedirectView getRedirectView(final PaymentsDetailsRequest detailsRequest) throws ApiException, IOException {
-        log.info("REST request to handle payment redirect {}", detailsRequest);
-        var response = checkout.paymentsDetails(detailsRequest);
-        var redirectURL = "/result/";
-        switch (response.getResultCode()) {
-            case AUTHORISED:
-                redirectURL += "success";
-                break;
-            case PENDING:
-            case RECEIVED:
-                redirectURL += "pending";
-                break;
-            case REFUSED:
-                redirectURL += "failed";
-                break;
-            default:
-                redirectURL += "error";
-                break;
-        }
-        return new RedirectView(redirectURL + "?reason=" + response.getResultCode());
-    }
-
-    /* ################# UTILS ###################### */
-    private String findCurrency(String type) {
-        switch (type) {
-            case "paypal":
-            case "ach":
-                return "USD";
-            case "wechatpayqr":
-            case "alipay":
-                return "CNY";
-            case "dotpay":
-                return "PLN";
-            case "boletobancario":
-            case "boletobancario_santander":
-                return "BRL";
-            default:
-                return "EUR";
-        }
-    }
-    /* ################# end UTILS ###################### */
 }
--- src/main/resources/static/adyenImplementation.js
+++ src/main/resources/static/adyenImplementation.js
@@ -2,89 +2,123 @@ const clientKey = document.getElementById("clientKey").innerHTML;
 const type = document.getElementById("type").innerHTML;
 
 async function initCheckout() {
-  try {
-    const paymentMethodsResponse = await callServer("/api/getPaymentMethods");
-    const configuration = {
-      paymentMethodsResponse: filterUnimplemented(paymentMethodsResponse),
-      clientKey,
-      locale: "en_US",
-      environment: "test",
-      showPayButton: true,
-      paymentMethodsConfiguration: {
-        ideal: {
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const sessionId = urlParams.get('sessionId');
+  const redirectResult = urlParams.get('redirectResult');
+
+  if(!sessionId){
+    try {
+      const checkoutSessionResponse = await callServer("/api/sessions?type=" + type);
+      const configuration = {
+        clientKey,
+        locale: "en_US",
+        environment: "test",
+        session: checkoutSessionResponse,
+        showPayButton: true,
+        paymentMethodsConfiguration: {
+          hasHolderName: true,
+          holderNameRequired: true,
+          billingAddressRequired: true,
+          ideal: {
+            showImage: true,
+          },
+          card: {
+            hasHolderName: true,
+            holderNameRequired: true,
+            name: "Credit or debit card",
+            amount: {
+              value: 1000,
+              currency: "EUR",
+            },
+          },
+          paypal: {
+            amount: {
+              value: 1000,
+              currency: "USD",
+            },
+            environment: "test", // Change this to "live" when you're ready to accept live PayPal payments
+            countryCode: "US", // Only needed for test. This will be automatically retrieved when you are in production.
+          }
+        },
+        onPaymentCompleted: (result, component) => {
+          console.info("onPaymentCompleted");
+          console.info(result, component);
+          handleServerResponse(result, component)
+        },
+        onError: (error, component) => {
+          console.error("onError");
+          console.error(error.name, error.message, error.stack, component);
+          handleServerResponse(error, component)
+        },
+      };
+      // `spring.jackson.default-property-inclusion=non_null` needs to set in
+      // src/main/resources/application.properties to avoid NPE here
+      const checkout = await new AdyenCheckout(configuration);
+      checkout.create(type).mount(document.getElementById("payment"));
+    } catch (error) {
+      console.error("mounting error");
+
+      console.error(error);
+      alert("Error occurred. Look at console for details");
+    }
+
+  }
+  else{
+    try {
+      const configuration = {
+        clientKey,
+          locale: "en_US",
+        environment: "test",
+        session: {
+          id: sessionId, // Unique identifier for the payment session.
+        },
+        showPayButton: true,
+        paymentMethodsConfiguration: {
+        hasHolderName: true,
+          holderNameRequired: true,
+          billingAddressRequired: true,
+          ideal: {
           showImage: true,
         },
         card: {
           hasHolderName: true,
-          holderNameRequired: true,
-          name: "Credit or debit card",
-          amount: {
+            holderNameRequired: true,
+            name: "Credit or debit card",
+            amount: {
             value: 1000,
-            currency: "EUR",
+              currency: "EUR",
           },
         },
         paypal: {
           amount: {
             value: 1000,
-            currency: "USD",
+              currency: "USD",
           },
           environment: "test", // Change this to "live" when you're ready to accept live PayPal payments
-          countryCode: "US", // Only needed for test. This will be automatically retrieved when you are in production.
-          onCancel: (data, component) => {
-            component.setStatus('ready');
-          },
+            countryCode: "US", // Only needed for test. This will be automatically retrieved when you are in production.
         }
       },
-      onSubmit: (state, component) => {
-        if (state.isValid) {
-          handleSubmission(state, component, "/api/initiatePayment");
-        }
+        onPaymentCompleted: (result, component) => {
+          console.info(result, component);
+          handleServerResponse(result, component)
+        },
+          onError: (error, component) => {
+        console.error(error.name, error.message, error.stack, component);
+        handleServerResponse(error, component)
       },
-      onAdditionalDetails: (state, component) => {
-        handleSubmission(state, component, "/api/submitAdditionalDetails");
-      },
-    };
-    // `spring.jackson.default-property-inclusion=non_null` needs to set in
-    // src/main/resources/application.properties to avoid NPE here
-    const checkout = await new AdyenCheckout(configuration);
-    checkout.create(type).mount(document.getElementById("payment"));
-  } catch (error) {
-    console.error(error);
-    alert("Error occurred. Look at console for details");
+      };
+
+      const checkout = await AdyenCheckout(configuration);
+
+      checkout.submitDetails({details: {redirectResult}});
+    } catch (error) {
+      console.error(error);
+      alert("Error occurred. Look at console for details");
+    }
   }
 }
 
-function filterUnimplemented(pm) {
-  pm.paymentMethods = pm.paymentMethods.filter((it) =>
-    [
-      "scheme",
-      "ideal",
-      "dotpay",
-      "giropay",
-      // "sepadirectdebit",
-      "directEbanking",
-      "ach",
-      "alipay",
-      "klarna_paynow",
-      "klarna",
-      "klarna_account",
-      "paypal",
-    ].includes(it.type)
-  );
-  return pm;
-}
-
-// Event handlers called when the shopper selects the pay button,
-// or when additional information is required to complete the payment
-async function handleSubmission(state, component, url) {
-  try {
-    const res = await callServer(url, state.data);
-    handleServerResponse(res, component);
-  } catch (error) {
-    console.error(error);
-    alert("Error occurred. Look at console for details");
-  }
-}
 
 // Calls your server endpoints
 async function callServer(url, data) {
@@ -99,11 +133,7 @@ async function callServer(url, data) {
   return await res.json();
 }
 
-// Handles responses sent from your server to the client
 function handleServerResponse(res, component) {
-  if (res.action) {
-    component.handleAction(res.action);
-  } else {
     switch (res.resultCode) {
       case "Authorised":
         window.location.href = "/result/success";
@@ -119,7 +149,6 @@ function handleServerResponse(res, component) {
         window.location.href = "/result/error";
         break;
     }
-  }
 }
 
 initCheckout();
