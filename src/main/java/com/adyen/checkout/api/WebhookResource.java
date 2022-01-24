9d64770c8b1f03110d9599e996a1d334f20eedf0
gcatanese <gcatanese@yahoo.com>
2022-01-24 11:00:31 +0100

    Enable HMAC validation, refactor to use properties from ApplicationProperties

--- src/main/java/com/adyen/checkout/api/WebhookResource.java
+++ src/main/java/com/adyen/checkout/api/WebhookResource.java
@@ -1,58 +1,63 @@
 package com.adyen.checkout.api;
 
+import com.adyen.checkout.ApplicationProperty;
 import com.adyen.model.notification.NotificationRequest;
 import com.adyen.model.notification.NotificationRequestItem;
 import com.adyen.util.HMACValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.SignatureException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
- * REST controller for receiving Adyen webhooks
+ * REST controller for receiving Adyen webhook notifications
  */
 @RestController
 @RequestMapping("/api")
 public class WebhookResource {
     private final Logger log = LoggerFactory.getLogger(WebhookResource.class);
 
-    @Value("${ADYEN_HMAC_KEY:#{null}}")
-    private String hmacKey;
+    private ApplicationProperty applicationProperty;
 
-    private final List<NotificationRequestItem> notificationItems = new ArrayList<>();
+    @Autowired
+    public WebhookResource(ApplicationProperty applicationProperty) {
+        this.applicationProperty = applicationProperty;
 
-    // Providing an endpoint to show all received webhooks. You don't want to do that on production
-    @GetMapping("webhooks")
-    public List<NotificationRequestItem> getItems(){
-        return notificationItems;
+        if(this.applicationProperty.getHmacKey() == null) {
+            log.warn("ADYEN_HMAC_KEY is UNDEFINED (Webhook cannot be authenticated)");
+            throw new RuntimeException("ADYEN_HMAC_KEY is UNDEFINED");
+        }
     }
 
-    @PostMapping("/webhooks/notifications")
+    /**
+     * Process incoming Webhook notifications
+     * @param notificationRequest
+     * @return
+     */
+    @PostMapping("/webhook/notifications")
     public ResponseEntity<String> webhooks(@RequestBody NotificationRequest notificationRequest){
 
         notificationRequest.getNotificationItems().forEach(
           item -> {
-              notificationItems.add(item); // Populating the view list
-
-              // We recommend to activate HMAC validation in the webhooks for security reasons
-//              try {
-//                  if (new HMACValidator().validateHMAC(item, hmacKey)) {
+              // We recommend validate HMAC signature in the webhooks for security reasons
+              try {
+                  if (new HMACValidator().validateHMAC(item, this.applicationProperty.getHmacKey())) {
                       log.info("Received webhook with event {} : \n" +
                           "Merchant Reference: {}\n" +
                           "Alias : {}\n" +
                           "PSP reference : {}"
                           , item.getEventCode(), item.getMerchantReference(), item.getAdditionalData().get("alias"), item.getPspReference());
-//                  } else {
-//                      log.warn("Could not validate HMAC Key for incoming webhook message. Have you set the environment variable?");
-//                  }
-//              } catch (SignatureException e) {
-//                  log.error("Error while validating HMAC Key", e);
-//              }
+                  } else {
+                      // invalid HMAC signature: do not send [accepted] response
+                      log.warn("Could not validate HMAC signature for incoming webhook message: {}", item);
+                      throw new RuntimeException("Invalid HMAC signature");
+                  }
+              } catch (SignatureException e) {
+                  log.error("Error while validating HMAC Key", e);
+              }
           }
         );
 
