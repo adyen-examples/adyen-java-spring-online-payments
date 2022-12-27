713400905a5666d6c0360545b6fb5f09fa0e88d6
Beppe Catanese <1771700+gcatanese@users.noreply.github.com>
2022-12-27 21:07:51 +0100

    Upgrade SpringBoot v3 Java 17 (#44)
    * Upgrade gradle v7.4
    
    * Upgrade SpringBoot3 and Java 17
    
    * Update imports (support SpringBoot v3)
    
    * Support Java 17

--- src/main/java/com/adyen/checkout/OnlinePaymentsApplication.java
+++ src/main/java/com/adyen/checkout/OnlinePaymentsApplication.java
@@ -1,12 +1,12 @@
 package com.adyen.checkout;
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class OnlinePaymentsApplication {
