41d90f3ffb839921bc9cca25bbfecbddd1b2a2e0
jillingk <93914435+jillingk@users.noreply.github.com>
2023-04-14 13:37:04 +0200

    fix serialization issue by importing gson (#70)

--- checkout-example/src/main/java/com/adyen/checkout/OnlinePaymentsApplication.java
+++ checkout-example/src/main/java/com/adyen/checkout/OnlinePaymentsApplication.java
@@ -6,10 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
 
-@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
+@SpringBootApplication
 public class OnlinePaymentsApplication {
 
     private static final Logger log = LoggerFactory.getLogger(OnlinePaymentsApplication.class);
