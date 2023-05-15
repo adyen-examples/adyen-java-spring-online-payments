3cdd08ffad1bbd478bd58e38472ba9d4d784309c
gcatanese <gcatanese@yahoo.com>
2023-05-15 12:51:02 +0200

    Refactor to use model built-in methods

--- subscription-example/src/main/java/com/adyen/checkout/api/WebhookResource.java
+++ subscription-example/src/main/java/com/adyen/checkout/api/WebhookResource.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.security.SignatureException;
 
 /**
@@ -35,14 +36,17 @@ public class WebhookResource {
     }
 
     /**
-     * Process incoming Webhook notification: get NotificationRequestItem, validate HMAC signature,
+     * Process the incoming Webhook: get NotificationRequestItem, validate HMAC signature,
      * consume the event asynchronously, send response ["accepted"]
      *
-     * @param notificationRequest
+     *  @param json Payload of the webhook
      * @return
      */
     @PostMapping("/webhooks/notifications")
-    public ResponseEntity<String> webhooks(@RequestBody NotificationRequest notificationRequest) {
+    public ResponseEntity<String> webhooks(@RequestBody String json) throws IOException {
+
+        // from JSON string to object
+        var notificationRequest = NotificationRequest.fromJson(json);
 
         // fetch first (and only) NotificationRequestItem
         var notificationRequestItem = notificationRequest.getNotificationItems().stream().findFirst();
