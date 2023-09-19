c31ad2ff6856ce478e42b06964fd897d3dffd8f5
Beppe Catanese <gcatanese@yahoo.com>
2023-09-19 19:35:52 +0200

    Deserialize json

--- checkout-example-advanced/src/main/java/com/adyen/checkout/api/WebhookResource.java
+++ checkout-example-advanced/src/main/java/com/adyen/checkout/api/WebhookResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.security.SignatureException;
 
 /**
@@ -40,11 +41,14 @@ public class WebhookResource {
      * Process incoming Webhook notification: get NotificationRequestItem, validate HMAC signature,
      * consume the event asynchronously, send response ["accepted"]
      *
-     * @param notificationRequest
+     * @param json Payload of the webhook event
      * @return
      */
     @PostMapping("/webhooks/notifications")
-    public ResponseEntity<String> webhooks(@RequestBody NotificationRequest notificationRequest) {
+    public ResponseEntity<String> webhooks(@RequestBody String json) throws IOException {
+
+        // from JSON string to object
+        var notificationRequest = NotificationRequest.fromJson(json);
 
         // fetch first (and only) NotificationRequestItem
         var notificationRequestItem = notificationRequest.getNotificationItems().stream().findFirst();
