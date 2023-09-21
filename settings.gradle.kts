9d0f51a9ed35d72f26ab07b81a90734bcdf7bd71
Julien Lengrand-Lambert <julien@lengrand.fr>
2023-09-21 17:19:32 +0200

    Add multiproject setup

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "adyen-sample-multiproject"
+
+include("checkout-example")
+include("checkout-example-advanced")
+include("gitcard-example")
+include("giving-example")
+include("paybylink-example")
+include("subscription-example")
