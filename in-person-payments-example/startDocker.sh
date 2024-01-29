676ae71bd00e6ffb91ef7e4bb0a10a4630b904aa
Kwok He Chu <105217051+Kwok-he-Chu@users.noreply.github.com>
2024-01-29 20:29:09 +0100

    Fix docker file

--- in-person-payments-example/startDocker.sh
+++ in-person-payments-example/startDocker.sh
@@ -1,4 +1,4 @@
 docker run \
 -e ADYEN_HMAC_KEY \
 -e ADYEN_API_KEY \
--p8080:8080 adyen-java-spring-in-person-payments-example:latest
+-p 8080:8080 adyen-java-spring-in-person-payments-example:latest
