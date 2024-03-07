9542ad8c1c6deb190c850176c2c439f4d1ca22fd
gcatanese <gcatanese@yahoo.com>
2024-03-07 16:24:38 +0100

    Import HttpStatus

--- authorisation-adjustment-example/src/main/java/com/adyen/checkout/api/WebhookController.java
+++ authorisation-adjustment-example/src/main/java/com/adyen/checkout/api/WebhookController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
